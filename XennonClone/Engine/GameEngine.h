ab2187fe524a5e8da39e588909a0325ee997f375
David Santos <39455909+DavidSantosDEV@users.noreply.github.com>
2022-11-03 20:52:18 +0000

    Delta time and framecap

--- XennonClone/Engine/GameEngine.h
+++ XennonClone/Engine/GameEngine.h
@@ -6,8 +6,11 @@ class RenderComponent;
 class GameObject;
 class GameWorld;
 
+
 class GameEngine
 {
+
+	
 public:
 	~GameEngine();
 	void Init(const char* windowTitle, int windowWidth, int windowHeight, GameWorld* World);
@@ -22,6 +25,9 @@ public:
 	void AddGameObjectToStack(GameObject* gameObject);
 	void RemoveObjectFromStack(GameObject* gameObject);
 
+
+	
+
 private:
 	std::vector<GameObject*> m_GameObjectStack = {};
 	std::vector<std::shared_ptr<RenderComponent>> m_RenderComponents = {};
@@ -30,7 +36,13 @@ private:
 	class SDLWrapper* m_Sdl;
 	class Window* m_Window;
 
+	const int m_MaxFPS = 60;
+	
+	static float m_ElapsedMS;
+
 public:
 	static GameWorld* GetGameWorld() { return m_World; }
+
+	static float GetDeltaTime() { return m_ElapsedMS; }
 };
 
